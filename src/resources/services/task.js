851bba0478783baf1b75f53ed0b24cf6e9ccb45f
wizisergio <wizisergio@gmail.com>
2025-08-04 09:53:05 +0200

    Add system module

--- src/resources/services/task.js
+++ src/resources/services/task.js
@@ -1,13 +1,16 @@
+import fetch from 'node-fetch';
+
 const URL = "http://localhost:3000/task";
 
 
 
 // Create
 export async function createTask(task) {
-    const res = await fetch( URL, {
+    const res = await fetch(URL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(query)
+        body: JSON.stringify(task)
     });
     const data = await res.json();
-};
+    return data;
+}
