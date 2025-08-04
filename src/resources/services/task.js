375514a71627e1877e365086b002da87bac78362
wizisergio <wizisergio@gmail.com>
2025-08-04 14:10:25 +0200

    Update task.js

--- src/resources/services/task.js
+++ src/resources/services/task.js
@@ -14,3 +14,41 @@ export async function createTask(task) {
     const data = await res.json();
     return data;
 }
+
+// Read
+export async function getAllTasks() {
+    const res = await fetch(URL);
+    const data = await res.json();
+    return data;
+};
+
+// Read [taskId]
+export async function getTaskById(taskId) {
+    const res = await fetch(`${URL}/${taskId}`);
+    const data = await res.json();
+    return data;
+};
+
+// Read [listId]
+export async function getTaskByListId(listId) {
+    const res = await fetch(`${URL}?listId=${listId}`);
+    const data = await res.json();
+    return data;
+};
+
+// Update list
+export async function updateTask(id, updatingTask) {
+    const res = await fetch(`${URL}/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/JSON" },
+        body: JSON.stringify(updatingTask)
+    });
+    const data = await res.json();
+};
+
+// Delete list
+export async function deleteList(id) {
+    await fetch(`${URL}/${id}`, {
+        method: "DELETE"
+    });
+};
